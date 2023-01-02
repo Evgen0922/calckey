31a73fd2523b4a9178954e93125c67ea76ba105b
Kevgen <87700221+Evgen0922@users.noreply.github.com>
2023-01-02 12:12:14 +0300

    update

--- install.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-pushd "%~dp0\bin\Apache\bin\"
-"%~dp0\bin\PHP\php.exe" -c "%~dp0\bin\PHP\php.default.ini" "%~dp0\install.php"
-popd
--- stream.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-pushd "%~dp0\bin\Apache\bin\"
-"%~dp0\bin\PHP\php.exe" -c "%~dp0\bin\PHP\php.ini" "%~dp0\modules\stream.php" %*
-popd
